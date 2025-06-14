e32630a9e43fb6e341df2a72e6fe97b0924f10fb
Yunviko <s30357@pjwstk.edu.pl>
2025-06-14 19:48:05 +0200

    health

--- Assets/Scripts/HelathDisplay.cs
+++ Assets/Scripts/HelathDisplay.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelathDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int health;
+    public int maxHealth;
 
-    // Update is called once per frame
+    public Sprite emptyHeart;
+    public Sprite fullHearts;
+    public Image[] hearths;
+    
+
+    void Start() 
+    { }
+    
+
     void Update()
     {
-        
+        for (int i = 0; i < hearths.Length; i++)
+        {
+            if (i < health)
+            {
+                hearths[i].sprite = fullHearts;
+            }
+            else 
+            {
+                hearths[i].sprite = emptyHeart;
+            }
+            if(i<maxHealth)
+            {
+                hearths[i].enabled=true;             
+            }
+            else
+            {
+                hearths[i].enabled=false;
+            }
+        }
     }
+    
+
+
+
+
+    
 }
